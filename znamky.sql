f4544c11e4324d76ffa3a170328a0832da2ecdad
pavli47 <96058666+pavli47@users.noreply.github.com>
2022-12-01 18:55:34 +0100

    Update znamky.sql

--- znamky.sql
+++ znamky.sql
@@ -57,26 +57,26 @@ UNIQUE(trieda,predmet),
 PRIMARY KEY (trieda,predmet));
 
 
-INSERT INTO student VALUES(1,'Anna', 'Abová', 'Ž', '1.A', '2004-12-12');
-INSERT INTO student VALUES(2,'Bruno', 'Bell', 'M', '2.B', '2004-11-01');
-INSERT INTO student VALUES(3,'Cecil', 'Cinkota', 'M', '3.C', '2003-05-28');
-INSERT INTO student VALUES(4,'Diana', 'Držková', 'Ž', '1.A', '2004-08-20');
+INSERT INTO student(meno,priezvisko,pohlavie,trieda,datum_nar) VALUES('Anna', 'Abová', 'Ž', '1.A', '2004-12-12');
+INSERT INTO student(meno,priezvisko,pohlavie,trieda,datum_nar) VALUES('Bruno', 'Bell', 'M', '2.B', '2004-11-01');
+INSERT INTO student(meno,priezvisko,pohlavie,trieda,datum_nar) VALUES('Cecil', 'Cinkota', 'M', '3.C', '2003-05-28');
+INSERT INTO student(meno,priezvisko,pohlavie,trieda,datum_nar) VALUES('Diana', 'Držková', 'Ž', '1.A', '2004-08-20');
 
-INSERT INTO ucitel VALUES(1,'Žaneta', 'Žilková', 'Ž');
-INSERT INTO ucitel VALUES(2,'Xénia', 'Xindl', 'Ž');
-INSERT INTO ucitel VALUES(3,'William', 'Willis', 'M');
-INSERT INTO ucitel VALUES(4,'Viktor', 'Vanek', 'M');
+INSERT INTO ucitel(meno,priezvisko,pohlavie) VALUES('Žaneta', 'Žilková', 'Ž');
+INSERT INTO ucitel(meno,priezvisko,pohlavie) VALUES('Xénia', 'Xindl', 'Ž');
+INSERT INTO ucitel(meno,priezvisko,pohlavie) VALUES('William', 'Willis', 'M');
+INSERT INTO ucitel(meno,priezvisko,pohlavie) VALUES('Viktor', 'Vanek', 'M');
 
 INSERT INTO predmety VALUES('Matematika', 'MAT');
 INSERT INTO predmety VALUES('Angličtina', 'ENG');
 INSERT INTO predmety VALUES('Slovenčina', 'SJL');
 
 
-INSERT INTO znamky VALUES('1',1, 1, 'MAT',  '13:30', '2021-12-19', 'uloha', 1);
-INSERT INTO znamky VALUES('1',1, 2, 'SJL',  '12:30', '2021-12-18', 'pisomka', 2);
-INSERT INTO znamky VALUES('1',2, 1, 'MAT',  '12:00', '2021-12-17', 'test', 2);
-INSERT INTO znamky VALUES('4',3, 3, 'ENG',  '08:30', '2021-12-11', 'test', 2);
-INSERT INTO znamky VALUES('pochvala',3, 3, 'ENG',  '09:30', '2021-12-11', 'konverzácia', 0.5);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet,cas,datum,pozn, vaha) VALUES ('1',1, 1, 'MAT',  '13:30', '2021-12-19', 'uloha', 1);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet,cas,datum,pozn, vaha) VALUES('1',1, 2, 'SJL',  '12:30', '2021-12-18', 'pisomka', 2);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet,cas,datum,pozn, vaha) VALUES('1',2, 1, 'MAT',  '12:00', '2021-12-17', 'test', 2);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet,cas,datum,pozn, vaha) VALUES('4',3, 3, 'ENG',  '08:30', '2021-12-11', 'test', 2);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet,cas,datum,pozn, vaha) VALUES('pochvala',3, 3, 'ENG',  '09:30', '2021-12-11', 'konverzácia', 0.5);
 
 INSERT INTO triedy  VALUES('1.A', 'MAT');
 INSERT INTO triedy  VALUES('1.A', 'SJL');
@@ -112,5 +112,14 @@ UPDATE ucitel SET prihlasovacie_meno = 'zanetaz' where ucitel_id =1;
 UPDATE ucitel SET prihlasovacie_meno = 'xeniax' where ucitel_id =2;
 UPDATE ucitel SET prihlasovacie_meno = 'wiliamw' where ucitel_id =3;
 UPDATE ucitel SET prihlasovacie_meno = 'viktorv' where ucitel_id =4;
+
+INsert INTO triedy values('1.A', 'MAT');
+INSERT INTO student values(5,'Maria', 'Minariková', 'ano','4.S', '1900-01-01');
+UPDATE  student  set pohlavie = 'lol' where meno = 'Anna';
+INSERT INTO student values(5,NULL, 'Minariková', 'Ž','4.S', '1900-01-01');
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet, vaha) values('1',8,1,'ENG',1);
+INSERT INTO znamky(znamka,student_id,ucitel_id,predmet, vaha) values('1',1,1,'DEJ',1);
+
+
 
 
