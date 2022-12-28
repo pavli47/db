daff0a014e56101730bfb4b29bc74a37875240fb
pavli47 <96058666+pavli47@users.noreply.github.com>
2022-12-29 00:43:59 +0100

    Update du.sql

--- du.sql
+++ du.sql
@@ -98,53 +98,53 @@ idtry INT,
 );
 
 
-insert into students values('Anna', 'Adams');
-insert into students values('Bruno', 'Bills');
-insert into students values('Charlie', 'Collins');
+insert into students(name,lastname) values('Anna', 'Adams');
+insert into students(name,lastname) values('Bruno', 'Bills');
+insert into students(name,lastname) values('Charlie', 'Collins');
 
-insert into professors values('Maria', 'Moralez');
-insert into students values('Nina', 'Nixon');
-insert into students values('Oliver', 'Olsen');
+insert into professors(name,lastname) values('Maria', 'Moralez');
+insert into students(name,lastname) values('Nina', 'Nixon');
+insert into students(name,lastname) values('Oliver', 'Olsen');
 
-insert into tests values(1, 'Spanish Unit 1');
-insert into tests values(2, 'Slovak');
-insert into tests values(3, 'Math1');
-insert into tests values(3, 'Math2');
-insert into tests values(10, 'error');
+insert into tests(author,name) values(1, 'Spanish Unit 1');
+insert into tests(author,name) values(2, 'Slovak');
+insert into tests(author,name) values(3, 'Math1');
+insert into tests(author,name) values(3, 'Math2');
+insert into tests(author,name) values(10, 'error');
 
 '
-insert into questions(1,1,'dog' , 'A', 'perro', 'caro','pero','gato');
-insert into questions(1,2,'but' , 'B', 'perro', 'pero','lero','gato');
-insert into questions(1,3,'hi' , 'C', 'perro', 'pero','hola','gato');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(1,1,'dog' , 'A', 'perro', 'caro','pero','gato');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(1,2,'but' , 'B', 'perro', 'pero','lero','gato');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(1,3,'hi' , 'C', 'perro', 'pero','hola','gato');
 
-insert into questions(2,1,'hi' , 'C', 'ale', 'nie','ahoj','gate');
-insert into questions(2,2,'yes' , 'A', 'ano', 'pero','nie','gate');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(2,1,'hi' , 'C', 'ale', 'nie','ahoj','gate');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(2,2,'yes' , 'A', 'ano', 'pero','nie','gate');
 
-insert into questions(3,1,'2+1' , 'A', '3', '4','5','6');
-insert into questions(3,2,'2*1' , 'B', '3', '2','22','6');
-insert into questions(3,3,'2/1' , 'D', '3', '4','5','2');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(3,1,'2+1' , 'A', '3', '4','5','6');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(3,2,'2*1' , 'B', '3', '2','22','6');
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(3,3,'2/1' , 'D', '3', '4','5','2');
 
-insert into questions(14,1,'error');
-
-
-
-insert into assignments(1,1,'2022-12-12', 1);
-insert into assignments(1,2,'2022-12-10', 2);
-insert into assignments(2,3,'2022-11-10', 3);
-insert into assignments(3,1,'2022-11-10', 4); /*error*/
-insert into assignments(1,1,'2022-11-10', 1); /*error*/
-insert into assignments(5,1,'2022-11-10', 2); /*error*/
+insert into questions(testid,questionnumber,question,correct,A,B,C,D) values(14,1,'error');
 
 
-insert into tries(1);
-insert into tries(2);
-insert into tries(3);
 
-insert into answears(1,1,'A');
-insert into answears(1,2,'B');
-insert into answearss(1,3,'C');
-insert into answears(2,1,'B');
-insert into answears(2,2,'A');
-insert into answearss(3,1,'A');
-insert into answearss(4); /*error*/
+insert into assignments(fromprof,tostudent,date ,test) values(1,1,'2022-12-12', 1);
+insert into assignments(fromprof,tostudent,date ,test) values(1,2,'2022-12-10', 2);
+insert into assignments(fromprof,tostudent,date ,test) values(2,3,'2022-11-10', 3);
+insert into assignments(fromprof,tostudent,date ,test) values(3,1,'2022-11-10', 4); /*error*/
+insert into assignments(fromprof,tostudent,date ,test) values(1,1,'2022-11-10', 1); /*error*/
+insert into assignments(fromprof,tostudent,date ,test) values(5,1,'2022-11-10', 2); /*error*/
+
+
+insert into tries(assignmentid) values(1);
+insert into tries(assignmentid) values(2);
+insert into tries(assignmentid) values(3);
+
+insert into answears(idtry ,questionnumber ,answer) values(1,1,'A');
+insert into answears(idtry ,questionnumber ,answer) values(1,2,'B');
+insert into answears(idtry ,questionnumber ,answer) values(1,3,'C');
+insert into answears(idtry ,questionnumber ,answer) values(2,1,'B');
+insert into answears(idtry ,questionnumber ,answer) values(2,2,'A');
+insert into answears(idtry ,questionnumber ,answer) values(3,1,'A');
+insert into answears(idtry ,questionnumber ,answer) values(4); /*error*/
 
