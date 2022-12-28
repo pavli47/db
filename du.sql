fd8432bd4adb7088bfcb5a3db6922d3ca2df6f2d
pavli47 <96058666+pavli47@users.noreply.github.com>
2022-12-29 00:11:37 +0100

    Update du.sql

--- du.sql
+++ du.sql
@@ -14,7 +14,12 @@ drop table if exists tests;
 create table tests(
 id SERIAL PRIMARY KEY,
 author INT NOT NULL,
-name TEXT);
+name TEXT,
+    constraint prof
+        FOREIGN KEY author
+          REFERENCES professors(id) 
+           ON DELETE cascade
+           ON UPDATE cascade );
 
 drop table  if exists questions;
 create table questions(
@@ -27,22 +32,71 @@ B TEXT,
 C TEXT,
 D TEXT,
 PRIMARY KEY(testid, questionnumber),
+  CONSTRAINT test_x
+  FOREIGN KEY testid
+  REFERENCES tests(id)
+   ON DELETE cascade
+   ON UPDATE cascade
 );
 
 drop table if exists assignments;
 create table assignements(
-from INT NOT NULL,
+fromprof INT NOT NULL,
 to INT NOT NULL,
 date DATE NOT NULL,
 test INT  NOT NULL,
-PRIMARY KEY(test , to ));
+  assignemntid SERIAL,
+  PRIMARY KEY(test , to ),
+  
+  constraint pr
+  foreign key fromprof
+  references professors(id)
+  
+  foreign key to
+  references students(id)
+
+  constraint tst
+  foreign key test
+  referebces tests(id)
+   on delete cascade
+  on update cascade
+  
+
+)
+  ;
 
 drop table if exists tries; 
 create table tries(
-id SERIAL PRIMARY KEY,
+id SERIAL,
 assignmentid INT,
-date DATE,
-answers jsonb);
+  time timestamp without time zone NOT NULL
+   DEFAULT (current_timestamp AT TIME ZONE 'UTC'), 
+  PRIMARY KEY(assignmentid, time),
+  constraint ids
+  foreign key assignmentid
+  references assignments(assignemntid)
+     on delete cascade
+  on update cascade
+  
+  
+  
+  
+);
+
+
+drop table if exists answears ;
+create table answears(
+idtry INT,
+  questionnumber INT,
+  answer TEXT,
+  PRIMARY KEY(idtry, questionnumber),
+   constraint ids
+  foreign key idtry 
+  references tries(id)
+     on delete cascade
+  on update cascade
+);
+
 
 insert into students values("Anna", "Adams");
 insert into students values("Bruno", "Bills");
@@ -59,16 +113,38 @@ insert into tests values(3, "Math2");
 insert into tests values(10, "error");
 
 
-insert into questions(1,1,"dog" , "A", "perro", "caro","pero","gato");
-insert into questions(1,2,"but" , "B", "perro", "pero","lero","gato");
-insert into questions(1,3,"hi" , "C", "perro", "pero","hola","gato");
-insert into questions(2,1,"hi" , "C", "ale", "nie","ahoj","gate");
-insert into questions(2,2,"yes" , "A", "ano", "pero","nie","gate");
-insert into questions(3,1,"2+1" , "A", "3", "4","5","6");
-insert into questions(3,2,"2*1" , "B", "3", "2","22","6");
-insert into questions(3,3,"2/1" , "D", "3", "4","5","2");
+insert into questions(1,1,"dog" , 'A', "perro", "caro","pero","gato");
+insert into questions(1,2,"but" , 'B', "perro", "pero","lero","gato");
+insert into questions(1,3,"hi" , 'C', "perro", "pero","hola","gato");
+
+insert into questions(2,1,"hi" , 'C', "ale", "nie","ahoj","gate");
+insert into questions(2,2,"yes" , 'A', "ano", "pero","nie","gate");
+
+insert into questions(3,1,"2+1" , 'A', "3", "4","5","6");
+insert into questions(3,2,"2*1" , 'B', "3", "2","22","6");
+insert into questions(3,3,"2/1" , 'D', "3", "4","5","2");
+
 insert into questions(14,1,"error");
 
 
 
+insert into assignments(1,1,'2022-12-12', 1);
+insert into assignments(1,2,'2022-12-10', 2);
+insert into assignments(2,3,'2022-11-10', 3);
+insert into assignments(3,1,'2022-11-10', 4); /*error*/
+insert into assignments(1,1,'2022-11-10', 1); /*error*/
+insert into assignments(5,1,'2022-11-10', 2); /*error*/
+
+
+insert into tries(1);
+insert into tries(2);
+insert into tries(3);
+
+insert into answears(1,1,'A');
+insert into answears(1,2,'B');
+insert into answearss(1,3,'C');
+insert into answears(2,1,'B');
+insert into answears(2,2,'A');
+insert into answearss(3,1,'A');
+insert into answearss(4); /*error*/
 
